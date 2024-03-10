4bf9fd5a5815c51105be064c5a738fad7259f97b
Гриша Васильев <gregory.vasiliev@gmail.com>
2024-03-10 12:26:58 +0300

    Добавьте файлы проекта.

---
+++ лаб 2 задание 1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    std::ifstream file("a.txt");
+    if (!file.is_open()) {
+        std::cout << "Не удалось открыть файл a.txt" << std::endl;
+        return 1;
+    }
+
+    double number;
+    int countPositive = 0;
+    double sumPositive = 0.0;
+
+    while (file >> number) {
+        if (number > 0) {
+            countPositive++;
+            sumPositive += number;
+        }
+    }
+
+    file.close();
+
+    std::cout << "Количество положительных элементов: " << countPositive << std::endl;
+    std::cout << "Сумма положительных элементов: " << sumPositive << std::endl;
+
+    return 0;
+}
+
